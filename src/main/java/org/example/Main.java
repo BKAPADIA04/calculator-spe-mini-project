5da64f3e5cf813700c717787080c68f1565ad62e
Bhavya <bkapadia04@gmail.com>
2025-09-27 14:53:59 +0530

    Subtract

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,4 +7,8 @@ public class Main {
     public static int add(int a, int b) {
         return a + b;
     }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -13,4 +13,13 @@ class MainTest {
         assertEquals(15, Main.add(20, -5));
         assertEquals(5, Main.add(0, 5));
     }
+
+    @Test
+    void testSubtractNumbers() {
+        assertEquals(13799, Main.subtract(13910, 111));
+        assertEquals(5, Main.subtract(10, 5));
+        assertEquals(4, Main.subtract(-3, -7));
+        assertEquals(25, Main.subtract(20, -5));
+        assertEquals(-5, Main.subtract(0, 5));
+    }
 }
