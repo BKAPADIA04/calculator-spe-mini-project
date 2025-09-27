8a6471466a379e226497cf1fb8ca74fcee185769
Bhavya <bkapadia04@gmail.com>
2025-09-27 18:57:14 +0530

    Added More Tests

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -10,21 +10,25 @@ class MainTest {
     @Test
     void testSquareRoot() {
         assertEquals(5.0, calculator.squareRoot(25));
+        assertEquals(0.0, calculator.squareRoot(0));
     }
 
     @Test
     void testFactorial() {
         assertEquals(120, calculator.factorial(5));
+        assertEquals(3628800, calculator.factorial(10));
     }
 
     @Test
     void testNaturalLog() {
         assertEquals(2.302585092994046, calculator.naturalLog(10));
+        assertEquals(1.9459101490553132, calculator.naturalLog(7));
     }
 
     @Test
     void testPowerFunction() {
         assertEquals(8.0, calculator.powerFunction(2, 3));
+        assertEquals(0.25, calculator.powerFunction(2, -2));
     }
 
 
